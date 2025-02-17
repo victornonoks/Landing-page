ce5c788ab0d3e39d0f597fd711de89b4779a0b47
FWashington1999 <F.Washington1999@outlook.com>
2025-02-17 17:56:46 -0300

    projeto finalizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,13 +13,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 //botao para redirecionar whatsapp
 function botaoWpp() {
   //numero da empresa
-  const numero = "";
+  const numero = "5511947244958";
   //mensagem que vai ficar qnd o cliente for redirecionado pro wpp
   const mensagem = encodeURIComponent(
-    "Olá! Gostaria de mais informações sobre a fabrica."
+    "Olá! Gostaria de solicitar um orçamento."
   );
   //url do link do wpp com o numero e mensagem
-  const url = "";
+  const url = `https://wa.me/${numero}?text=${mensagem}`
 
   //abre a pagina do wpp em uma nova aba
   window.open(url, "_blank");
@@ -29,9 +29,9 @@ function menuShow() {
   let menuMobile = document.querySelector('.mobile-menu');
   if (menuMobile.classList.contains('open')) {
       menuMobile.classList.remove('open');
-      document.querySelector('.icon').src = "../imgs/menu_white_36dp.svg";
+      document.querySelector('.icon').src = "assets/imgs/menu_white_36dp.svg";
   } else {
       menuMobile.classList.add('open');
-      document.querySelector('.icon').src = "../imgs/close_white_36dp.svg";
+      document.querySelector('.icon').src = "assets/imgs/close_white_36dp.svg";
   }
 }
